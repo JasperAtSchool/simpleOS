23063df715fad028d3b32dd7fa8ad840ebacb704
DJj123dj <80536295+DJj123dj@users.noreply.github.com>
2021-11-11 20:40:12 +0100

    Update installer.lua

--- installer.lua
+++ installer.lua
@@ -76,7 +76,7 @@ shell.run("background","mkdir","./libs/cobalt/lib/modules/mouse")
 newload("creating ./libs/cobalt/lib/modules/graphics/classes",8)
 shell.run("background","mkdir","./libs/cobalt/lib/modules/graphics/classes")
 
-term.setCursorPos(((x/2 - 2) + index),(y/2 + 2))
+term.setCursorPos((x/2 - 2),(y/2 + 2))
 
 shell.run("background","wget","https://raw.githubusercontent.com/DJj123dj/testSystem/main/startup.lua","./startup.lua")
 
