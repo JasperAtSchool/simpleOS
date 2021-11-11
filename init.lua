09f5835c32213843d90fe2b9c39ca5f0c05b7265
DJj123dj <80536295+DJj123dj@users.noreply.github.com>
2021-11-11 09:54:57 +0100

    Update init.lua

--- init.lua
+++ init.lua
@@ -2,5 +2,6 @@ term.clear()
 local x,y = term.getSize()
 local text = "installer"
 print(string.len("textlenght: "..text))
+--hello
 term.setCursorPos(((x / 2) - string.len(text)),(y / 2))
 print(text)
