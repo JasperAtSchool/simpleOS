37d1bed47fe86a52396eca5fe39c6a6fb67fece6
DJj123dj <80536295+DJj123dj@users.noreply.github.com>
2021-11-11 10:21:36 +0100

    Update init.lua

--- init.lua
+++ init.lua
@@ -1,2 +1,5 @@
 fs.makeDir("./installer")
+if (fs.exists("./installer/installer.lua")) then
+    fs.delete('./installer/installer.lua')
+end
 shell.run("wget","https://raw.githubusercontent.com/DJj123dj/testSystem/main/installer.lua","./installer/installer.lua")
