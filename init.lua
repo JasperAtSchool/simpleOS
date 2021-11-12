f56e198c639299b562dfdf6be4bfc258259330bd
DJj123dj <80536295+DJj123dj@users.noreply.github.com>
2021-11-12 10:53:43 +0100

    Update init.lua

--- init.lua
+++ init.lua
@@ -4,5 +4,5 @@ end
 if (fs.exists("./installer/init.lua")) then
     fs.delete('./installer/init.lua')
 end
-shell.run("wget","https://raw.githubusercontent.com/DJj123dj/testSystem/main/init.lua","./installer/init.lua")
+shell.run("wget","https://raw.githubusercontent.com/DJj123dj/testSystem/main/setup/init.lua","./installer/init.lua")
 shell.run("./installer/init.lua")
