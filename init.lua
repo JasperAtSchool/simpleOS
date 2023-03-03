faf3f36f17f601eaa73697497d407347afbae33d
JasperAtSchool <126580902+JasperAtSchool@users.noreply.github.com>
2023-03-03 14:26:36 +0100

    Update init.lua

--- init.lua
+++ init.lua
@@ -4,5 +4,5 @@ end
 if (fs.exists("./installer/init.lua")) then
     fs.delete('./installer/init.lua')
 end
-shell.run("wget","https://raw.githubusercontent.com/DJj123dj/simpleOS/main/setup/init.lua","./installer/init.lua")
+shell.run("wget","https://raw.githubusercontent.com/JasperAtSchool/simpleOS/main/setup/init.lua","./installer/init.lua")
 shell.run("./installer/init.lua")
